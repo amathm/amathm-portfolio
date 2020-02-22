8908fef9ad981069831099dc3a3ebf6d98fbbe69
Mbaye <trpgm88@i07503.home>
2020-02-22 22:22:04 +0000

    banner component, added url bind of video src

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+  private videoUrl = ['../../assets/videos/Glitch - 27706-1080p.mp4'];
 
   constructor() { }
 
